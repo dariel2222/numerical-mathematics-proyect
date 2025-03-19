d79d0ffa2e9b16fc7a378782a73ee8bd2d3620a2
Dariel Sartorio <dariel2222@users.noreply.github.com>
2025-03-19 00:54:33 -0600

    Implementando Tabulate

--- Bisection.py
+++ Bisection.py
@@ -1,4 +1,10 @@
-# Programa que calcula el cero de la función: X^3 + X - 1, aplicando el Método de bisección 
+# Programa que calcula el cero de la función: X^3 + X - 1, aplicando el Método de bisección en un rango dado
+
+from tabulate import tabulate
+
+print("Programa que calcula el cero de la función: X^3 + X - 1, aplicando el Método de bisección en un rango dado")
+
+data = []
 
 i = 0
 a = 0
@@ -6,39 +12,31 @@ b = 1
 c = 0
 fa = 0
 fb = 0
-rango = 0.0005
+rango = 0.005
 tol=abs(b-a)
 
 while(abs(tol)>rango):
     
     i+=1
-    
-    print("|",i, " -->  ",end="")
-    print(" |a: ", a,end=" ")
-    print(" |b: ", b,end=" ")
-        
+         
     c=(a+b)/2
-    
-    print(" |c: ", c,end=" ")
     
     fa = pow(a,3)+a-1
     fb= pow(b,3)+b-1
     fc= pow(c,3)+c-1
    
-    print(" |f(a): ", fa,end=" ")
-    print(" |f(b): ", fb,end=" ")
-    print(" |f(c): ", fc,end=" ")
-
-    if(fa*fc>0):
+    data.append(i,a,b,c,fa,fb,fc,a,b,tol)
+   
+   # if(fa*fc>0):
         
-        a=c        
+    #    a=c        
     
-    else:
-        b=c
+    #else:
+     #   b=c
     
     tol = abs(b-a)
     
-    print(" |New a: ", a,end=" ")
-    print(" |New b: ", b,end=" ")
-    print(" |Tolerancia: ", tol)
+headers = ["I","a","b","c","f(a)","f(b)","f(c)","New a","New b","Tolerance"] 
+
+print(tabulate(data, headers=headers, tablefmt="grid"))   
         
