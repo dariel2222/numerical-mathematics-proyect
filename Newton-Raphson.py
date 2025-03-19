5ec97c50b48ad148c19d6c5ced399502ada10ce5
Dariel Sartorio <dariel2222@users.noreply.github.com>
2025-03-19 03:49:15 -0600

    crear variables y listas

--- Newton-Raphson.py
+++ Newton-Raphson.py
@@ -1,9 +1,17 @@
 # Programa que calcula el cero de la función: 3 - X - 2ln(x), aplicando el Método de Newton Raphson
 import math
 
+from tabulate import tabulate
+
+date = []
+header = ["I","Xn","f(Xn)","f'(Xn)","Xn+1"]
+
 i=0
 Xn=1.5
 fXn = 0
 fpXn = 0
 Xnplus = 0
+rango = 0.0005
 
+    
+    
