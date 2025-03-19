9a98e52409b4945529bbad1fd9a0b62a503bf0ca
Dariel Sartorio <dariel2222@users.noreply.github.com>
2025-03-19 04:16:15 -0600

    configurar bucle while

--- Newton-Raphson.py
+++ Newton-Raphson.py
@@ -8,10 +8,24 @@ header = ["I","Xn","f(Xn)","f'(Xn)","Xn+1"]
 
 i=0
 Xn=1.5
-fXn = 0
+fXn = Xn
 fpXn = 0
 Xnplus = 0
 rango = 0.0005
 
+while(fXn>rango):
+    
+    Xn = Xnplus
+    
+    fXn = 3 - 1.5 - 2(math.log(1.5,math.e))
+    
+    fpXn = - 1 - 2 / 1.5
+    
+    Xnplus = Xn-fXn/fpXn
+    
+    date.append([Xn,fXn,fpXn,Xnplusn])
+
+
+    
     
     
