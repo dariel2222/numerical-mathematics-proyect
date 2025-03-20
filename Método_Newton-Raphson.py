4099f118d2659f8e9c568577cc866e3dda130cf5
Dariel Sartorio <dariel2222@users.noreply.github.com>
2025-03-20 05:59:58 -0600

    Agregar Comentarios

--- Método_Newton-Raphson.py
+++ Método_Newton-Raphson.py
@@ -1,12 +1,21 @@
 # Programa que calcula el cero de la función: 3 - X - 2ln(x), aplicando el Método de Newton Raphson
+
+#inclución de librerias 
+
 import math
-from tabulate import tabulate
+from tabulate import tabulate 
+
+#Fución f(x)
 
 def f(x):
     return 3 - x - 2 * math.log(x,e)
 
+#Función f'(x)
+
 def f_prime(x):
     return -1 - 2 / x
+
+#Declaracion de variables y listas
 
 data = []
 headers = ["I","Xn","f(Xn)","f'(Xn)","Xn+1"]
@@ -15,6 +24,9 @@ i=1
 Xn=1.5
 rango = 0.0005
 e = math.e
+
+
+#Bucle para llevar a cabo el algoritmo necesario
 
 while True:
     
@@ -34,6 +46,8 @@ while True:
     i += 1
     
 
+
+#impresion por consola de la tabla de valores del Método Newton Raphson
 
 print(tabulate(data, headers=headers, tablefmt="rounded_grid"))    
     
